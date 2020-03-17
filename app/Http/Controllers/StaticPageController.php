2ca195990d8b8b79ce33185c2b94dd9dbb2f5b12
Gianufio83 <fiore.gianuario@gmail.com>
2020-03-17 16:30:35 +0100

    creazione file cartelle controller e view , header, footer, layout

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPageController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+
+    public function privacy()
+    {
+
+    }
+
+
+    public function faq()s
+    {
+
+    }
+}
