a3770df201cdcfe378b7853715f8ec88f0baa249
Gianufio83 <fiore.gianuario@gmail.com>
2020-03-18 16:36:31 +0100

    creazione home con css

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Gianuario\desktop\Boolean\laravel-boolean\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Gianuario\desktop\Boolean\laravel-boolean\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Gianuario\Desktop\boolean\laravel-boolean\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Gianuario\Desktop\boolean\laravel-boolean\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
